691ad00f1379aa59557dc27fe2c7bf2a2f9c923a
gwl6 <56182566+gwl6@users.noreply.github.com>
2019-11-04 21:51:28 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -53,7 +53,7 @@ jsproxy_config({
     /**
      * 默认节点
      */
-    node_default: 'f.gwl.workers.dev',
+    node_default: '当前站点',
     // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
     /**
