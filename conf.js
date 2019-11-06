a3f9bc187810ea79b27a8d3589a4a3d0103ad318
gwl6 <56182566+gwl6@users.noreply.github.com>
2019-11-06 09:45:31 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,6 +29,7 @@ jsproxy_config({
         'mysite': {
             label: '当前站点',
             lines: {
+                'da.last.workers.dev':2,
                 'f.gwl.workers.dev': 1,
             }
         },
@@ -38,6 +39,7 @@ jsproxy_config({
             hidden: true,
             lines: {
                 // 收费版（高权重）
+                'da.last.workers.dev':5,
                 'f.gwl.workers.dev': 4,
                 // 免费版（低权重，分摊一些成本）
                 // 每个账号每天 10 万次免费请求，但有频率限制
